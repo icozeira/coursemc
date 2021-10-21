ab2944ee687e549c7fd667cd6ccad6a2d36ed068
Paulo Cezar Rodrigues da Silva <engenheiro.paulo.cezar@gmail.com>
2021-10-21 14:42:29 -0400

    Endpoint/clientes/{id} disponível

---
+++ src/main/java/com/pcrs/coursemc/services/ClienteService.java
@@ -0,0 +1,26 @@
+package com.pcrs.coursemc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.pcrs.coursemc.domain.Cliente;
+import com.pcrs.coursemc.repositories.ClienteRepository;
+import com.pcrs.coursemc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repo;
+
+	
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				 "Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName())); 
+		
+	}
+	
+}
